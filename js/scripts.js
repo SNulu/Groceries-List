b965a90a5d8a74f29c4362ab1f87e411fdc1e43b
Jared Farkas and Sanyu Prado <jared@>
2019-01-24 13:46:10 -0800

    add id item.

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+  $("#formOne").submit(function(event){
+
+
+  event.preventDefault();
+});
+});
+
+
+/*
+var input = $("#sentence").val();
+var newArr = [];
+
+for (var i = 0; i < input.length; i++)
+{
+if (input[i] === "a" || input[i] === "e" || input[i] === "i" || input[i] === "o" || input[i] === "u"){
+ newArr.push("-");
+} else {
+	newArr.push(input[i]);
+}
+}
+var final = newArr.join("");
+$("#display").text(final).val();
+*/
